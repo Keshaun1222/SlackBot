4266e8e698d811aa621d3a9aeff68d5107dba73f
Keshaun <keshaun@eotir.com>
2016-07-02 05:53:26 -0400

    Allowed SpyInfo to take the Nation Name as an argument

--- src/Command/HelpCommand.php
+++ src/Command/HelpCommand.php
@@ -12,7 +12,7 @@ class HelpCommand extends Command {
         $message .= "!nation <nationid> - Lookup the stats of a nation.\r\n";
         $message .= "!raiding <nationid> [none|beige|vacation|both] - Lookup raiding targets with your nation's war range.\r\n";
         $message .= "!wars <nationid> - Gets the active wars for a nation.\r\n";
-        $message .= "!spyinfo <nationid> - Gets the recorded spy information for a nation\r\n";
+        $message .= "!spyinfo <nationid|name> - Gets the recorded spy information for a nation\r\n";
         $message .= "!addspy [nationid] <message> - Adds a spy report for a nation.\r\n";
 
         $client->getChannelGroupOrDMByID($this->channel)->then(function (ChannelInterface $channel) use ($client, $message) {
--- src/Command/SpyInfoCommand.php
+++ src/Command/SpyInfoCommand.php
@@ -11,9 +11,8 @@ class SpyInfoCommand extends Command{
         $db = $this->db;
 
         if (count($this->args) >= 1) {
-            $nationID = $this->args[0];
-            echo "Nation ID: " . $nationID . "\r\n";
-            if (is_numeric($nationID)) {
+            if (is_numeric($this->args[0])) {
+                $nationID = $this->args[0];
                 $nation = (new APICall())->call('nation', $nationID);
                 if (!isset($nation['error'])) {
                     $where = array('target_id' => 'int:' . $nationID);
@@ -44,14 +43,52 @@ class SpyInfoCommand extends Command{
                     });
                 }
             } else {
-                $message = "Invalid Arguments. Must use \"!spyinfo <nationid>\".";
+                /*$message = "Invalid Arguments. Must use \"!spyinfo <nationid>\".";
 
                 $client->getChannelGroupOrDMByID($this->channel)->then(function (ChannelInterface $channel) use ($client, $message) {
                     $client->send($message, $channel);
-                });
+                });*/
+
+                $nationName = $this->args[0];
+
+                $nationID = 0;
+                $nations = (new APICall())->call('nations');
+                foreach ($nations['nations'] as $check) {
+                    if ($check['nation'] == $nationName) {
+                        $nationID = $check['nationid'];
+                    }
+                }
+                if ($nationID != 0) {
+                    $where = array('target_id' => 'int:' . $nationID);
+                    $spies = $db->query('spy', $where);
+
+                    $message = $nationName . " Spy Information\r\n------------------------\r\n";
+                    foreach ($spies as $spy) {
+                        $date = new \DateTime($spy['timestamp'], new \DateTimeZone(getenv('TIMEZONE')));
+                        $date->setTimezone(new \DateTimeZone('Africa/Dakar'));
+                        $format = $date->format('(F d h:i A)');
+                        $message .= stripslashes($spy['message']) . " " . $format . "\r\n";
+                    }
+
+                    $this->client->getDMByUserId($this->userId)->then(function (DirectMessageChannel $dm) use ($client, $message) {
+                        $client->send($message, $dm);
+                    });
+
+                    if ($this->channel[0] != 'D') {
+                        $client->getChannelGroupOrDMByID($this->channel)->then(function (ChannelInterface $channel) use ($client) {
+                            $client->send("Please check your Direct Messages for list of recorded spy operations on this nation.", $channel);
+                        });
+                    }
+                } else {
+                    $message = "Nation does not exist. :pacman:\r\n";
+
+                    $client->getChannelGroupOrDMByID($this->channel)->then(function (ChannelInterface $channel) use ($client, $message) {
+                        $client->send($message, $channel);
+                    });
+                }
             }
         } else {
-            $message = "Missing Arguments. Must use \"!spyinfo <nationid>\".";
+            $message = "Missing Arguments. Must use \"!spyinfo <nationid|name>\".";
 
             $client->getChannelGroupOrDMByID($this->channel)->then(function (ChannelInterface $channel) use ($client, $message) {
                 $client->send($message, $channel);
