3970f94cd2d6a4843227c1a6dfd97495ea5b96cd
Keshaun <keshaun@eotir.com>
2016-07-27 10:42:16 -0400

    Testing linked nations on raiding command

--- src/Command/RaidingCommand.php
+++ src/Command/RaidingCommand.php
@@ -8,11 +8,18 @@ use SlackBot\APICall;
 class RaidingCommand extends Command {
     public function fire() {
         $client = $this->client;
+        $db = $this->db;
+        $user = $this->userId;
 
-        if (count($this->args) >= 1) {
-            $nationID = $this->args[0];
+        $where = array('user_id' => 'string:' . $user);
+
+        if (count($this->args) >= 1 || $db->query('link', $where)) {
+            $nationID = (count($this->args) >= 1 && is_numeric($this->args[0]) ? $this->args[0] : ($db->query('link', $where) ? $db->query('link', $where)[0]['nation_id'] : "Na"));
+            //if (count($this->args) >= 1 && (($this->args[0]))) {}
             if (is_numeric($nationID)) {
                 $nation = (new APICall())->call("nation", $nationID);
+
+                $param = (count($this->args >= 2) && is_numeric($this->args[0]) ? $this->args[1] : (count($this->args >= 1) ? $this->args[0] : "none"));
 
                 if (!isset($nation['error'])) {
                     $max = 1.75 * $nation['score'];
@@ -27,7 +34,7 @@ class RaidingCommand extends Command {
 
                     foreach ($nationsList['nations'] as $target) {
                         if ($target['allianceid'] == 0) {
-                            if (count($this->args) == 2 && strtolower($this->args[1]) == 'both') {
+                            if (strtolower($param) == 'both') {
                                 if ($target['score'] >= $min && $target['score'] <= $max) {
                                     if ($target['vacmode'] <= 24 && $target['vacmode'] > 0) {
                                         $wars = (new APICall())->call("war");
@@ -71,7 +78,7 @@ class RaidingCommand extends Command {
                                         }
                                     }
                                 }
-                            } else if (count($this->args) == 2 && strtolower($this->args[1]) == 'vacation') {
+                            } else if (strtolower($param) == 'vacation') {
                                 if ($target['score'] >= $min && $target['score'] <= $max && $target['color'] != 'beige') {
                                     if ($target['vacmode'] > 0) {
                                         if ($target['vacmode'] <= 24) {
@@ -99,7 +106,7 @@ class RaidingCommand extends Command {
                                         }
                                     }
                                 }
-                            } else if (count($this->args) == 2 && strtolower($this->args[1]) == 'beige') {
+                            } else if (strtolower($param) == 'beige') {
                                 if ($target['score'] >= $min && $target['score'] <= $max && $target['vacmode'] == 0) {
                                     if ($target['color'] == 'beige') {
                                         $data = (new APICall())->callFromWeb("https://politicsandwar.com/nation/id=" . $target['nationid']);
@@ -132,7 +139,7 @@ class RaidingCommand extends Command {
                                     }
                                 }
 
-                            } else if (count($this->args) == 1 || strtolower($this->args[1]) == 'none') {
+                            } else if (strtolower($param) == 'none') {
                                 if ($target['score'] >= $min && $target['score'] <= $max && $target['color'] != 'beige' && $target['vacmode'] == 0) {
                                     $wars = (new APICall())->call("war");
                                     $defenses = 0;
@@ -165,6 +172,12 @@ class RaidingCommand extends Command {
                         $client->send($message, $channel);
                     });
                 }
+            } else if($nationID == "na") {
+                $message = "No nation is linked to your slack account. Please use the *!link* command, or provide a nation id for *!raiding*.";
+
+                $client->getChannelGroupOrDMByID($this->channel)->then(function (ChannelInterface $channel) use ($client, $message) {
+                    $client->send($message, $channel);
+                });
             } else {
                 $message = "Invalid Arguments. Must use \"!raiding <nationid> [none|beige|vacation|both]\".";
 
